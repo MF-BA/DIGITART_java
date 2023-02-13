e74193c40f17cf9aea3ce5b161db4238f25cd7f5
MF-BA <fedi12.benaoun@gmail.com>
2023-02-13 23:20:23 +0100

    update7

---
+++ Auction_Management/src/javaapplication6/Conn.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+import java.sql.*;
+
+/**
+ *
+ * @author fedi1
+ */
+public class Conn {
+
+    static Conn A;
+
+    String URL = "jdbc:mysql://localhost/digitart";
+    String user = "root";
+    String pwd = "";
+
+    static public Connection conn;
+
+    private Conn() {
+
+        try {
+            conn = DriverManager.getConnection(URL, user, pwd);
+            System.out.println("Connection establised !!! ");
+        } catch (SQLException ex) {
+            System.err.println("Connection failed !!! ");
+        }
+    }
+
+    public static Connection getCon() {
+        if (A == null) {
+            A = new Conn();
+        }
+        return conn;
+    }
+
+}
