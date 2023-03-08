f9d46d9dcf3749c442867d98920979894cf58fe2
loukil33 <80423427+loukil33@users.noreply.github.com>
2023-03-08 01:20:18 +0100

    start edit profile interface

--- digitart/src/controller/Home_pageController.java
+++ digitart/src/controller/Home_pageController.java
@@ -20,8 +20,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -36,9 +43,67 @@ public class Home_pageController implements Initializable {
     @FXML
     private Label labelusername;
     @FXML
-    private Circle avatar_img;
-    @FXML
     private Button disconnect;
+    @FXML
+    private Pane avatar_icon;
+    @FXML
+    private Circle circle_image;
+    @FXML
+    private ImageView avatar_image;
+    @FXML
+    private AnchorPane home;
+    @FXML
+    private Button disconnect1;
+    @FXML
+    private Button editprof_btn;
+    @FXML
+    private TextField fname_editprof;
+    @FXML
+    private TextField lname_editprof;
+    @FXML
+    private TextField email_editprof;
+    @FXML
+    private DatePicker birth_d_editprof;
+    @FXML
+    private TextField cin_editprof;
+    @FXML
+    private TextField address_editprof;
+    @FXML
+    private TextField phone_num_editprof;
+    @FXML
+    private RadioButton male_gender_editprof;
+    @FXML
+    private RadioButton female_gender_editprof;
+    @FXML
+    private Button clear_fields_editprof;
+    @FXML
+    private ComboBox<?> Rolebox_editprof;
+    @FXML
+    private Label errormsglname_edit;
+    @FXML
+    private Label errormsgfname_edit;
+    @FXML
+    private Label errormsgemail_edit;
+    @FXML
+    private Label errormsggender_edit;
+    @FXML
+    private Label errormsgcin_edit;
+    @FXML
+    private Label errormsgaddress_edit;
+    @FXML
+    private Label errormsgbirthdate_edit;
+    @FXML
+    private Label errormsgphonenum_edit;
+    @FXML
+    private Button newpwd_btn;
+    @FXML
+    private Label errormsgfiiledit;
+    @FXML
+    private Circle circle_image_edit;
+    @FXML
+    private ImageView avatar_image1;
+    @FXML
+    private Label labelusername_edit;
 
     
     
@@ -50,7 +115,10 @@ public class Home_pageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         labelusername.setText(Data.user.getFirstname());
-        
+        if (Data.user.getImage()!=null){
+        Image image = new Image(Data.user.getImage());
+        circle_image.setFill(new ImagePattern(image));
+        }
      
         
     }    
@@ -70,6 +138,22 @@ public class Home_pageController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(Signin_pageController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    @FXML
+    private void editprof_btn(ActionEvent event) {
+    }
+
+    @FXML
+    private void clear_fields_update_btn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSelectImage(ActionEvent event) {
+    }
+
+    @FXML
+    private void newpwd_btn(ActionEvent event) {
     }
     
 }
